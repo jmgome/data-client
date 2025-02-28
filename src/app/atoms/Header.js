2b9dda0311fba752efc300b1d57057ba0eec93d9
junior Gomez <junior@ayte.com>
2025-02-27 19:35:37 -0500

    [FEATURE]added header

--- src/app/atoms/Header.js
+++ src/app/atoms/Header.js
@@ -1,15 +1,17 @@
-import React from 'react'
+import React from 'react';
+
 function getCurrentYear() {
-    return new Date().getFullYear();
-  }
-const Header = () => {
-    const year = getCurrentYear();
-  return (
-    <div className="w-full h-20 bg-lime-700 flex justify-center">
-      <h1>Subscriber user panel</h1>
-      <p>abonados estadio parque sur año {year}</p>
-    </div>
-  )
+  return new Date().getFullYear();
 }
 
-export default Header
+const Header = () => {
+  const year = getCurrentYear();
+  return (
+    <div className="w-full h-20 bg-lime-700 flex flex-col items-center justify-center">
+      <h1 className="text-xl font-bold text-white">Subscriber User Panel</h1>
+      <p className="text-white">Abonados Estadio Parque Sur - Año {year}</p>
+    </div>
+  );
+};
+
+export default Header;
